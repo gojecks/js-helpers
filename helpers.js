695b0f7c16458f4d1b509251b0112ec189ca514b
gojecks <gojeckskenwood@gmail.com>
2020-04-04 00:11:23 +0700

    updated files

--- helpers.js
+++ helpers.js
@@ -1,18 +1,18 @@
-import inarray from './src/utils/inarray';
-import isarray from './src/utils/isarray';
-import isboolean from './src/utils/isboolean';
-import isdefined from './src/utils/isdefined';
-import isdouble from './src/utils/isdouble';
-import isempty from './src/utils/isempty';
-import isequal from './src/utils/isequal';
-import isfloat from './src/utils/isfloat';
-import isfunction from './src/utils/isfunction';
-import isjsonstring from './src/utils/isjsonstring';
-import isnull from './src/utils/isnull';
-import isnumber from './src/utils/isnumber';
-import isobject from './src/utils/isobject';
-import isstring from './src/utils/isstring';
-import isundefined from './src/utils/isundefined';
+import inarray from './utils/inarray';
+import isarray from './utils/isarray';
+import isboolean from './utils/isboolean';
+import isdefined from './utils/isdefined';
+import isdouble from './utils/isdouble';
+import isempty from './utils/isempty';
+import isequal from './utils/isequal';
+import isfloat from './utils/isfloat';
+import isfunction from './utils/isfunction';
+import isjsonstring from './utils/isjsonstring';
+import isnull from './utils/isnull';
+import isnumber from './utils/isnumber';
+import isobject from './utils/isobject';
+import isstring from './utils/isstring';
+import isundefined from './utils/isundefined';
 
 export {
     inarray,
--- utils.js
+++ utils.js
@@ -1,19 +1,19 @@
-import base64 from './src/utils/base64';
-import camelcase from './src/utils/camelcase';
-import cookie from './src/utils/cookie';
-import copy from './src/utils/copy';
-import copyfrom from './src/utils/copyfrom';
-import count from './src/utils/count';
-import expect from './src/utils/expect';
-import extend from './src/utils/extend';
-import hashcode from './src/utils/hashcode';
-import logger from './src/utils/logger';
-import makeuid from './src/utils/makeuid';
-import nodubs from './src/utils/nodubs';
-import serialize from './src/utils/serializer';
-import splitntrim from './src/utils/splitntrim';
-import toobject from './src/utils/toobject';
-import unserialize from './src/utils/unserialize';
+import base64 from './utils/base64';
+import camelcase from './utils/camelcase';
+import cookie from './utils/cookie';
+import copy from './utils/copy';
+import copyfrom from './utils/copyfrom';
+import count from './utils/count';
+import expect from './utils/expect';
+import extend from './utils/extend';
+import hashcode from './utils/hashcode';
+import logger from './utils/logger';
+import makeuid from './utils/makeuid';
+import nodubs from './utils/nodubs';
+import serialize from './utils/serializer';
+import splitntrim from './utils/splitntrim';
+import toobject from './utils/toobject';
+import unserialize from './utils/unserialize';
 /**
  * helpers
  */
